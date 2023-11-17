c7c70945948561905ca6caa5437a68e5d2a806fe
studentAVDPR <studentAVDPR@outlook.com>
2023-11-17 06:45:07 -0800

    test

--- FileMethods.cs
+++ FileMethods.cs
@@ -57,7 +57,9 @@ namespace TollBooth
 
                         // Upload blob.
                         stream.Position = 0;
+
                         // TODO 7: Asynchronously upload the blob from the memory stream.
+                        await blob.UploadFromStreamAsync(stream);
                         // COMPLETE: await blob...;
 
                         successful = true;
